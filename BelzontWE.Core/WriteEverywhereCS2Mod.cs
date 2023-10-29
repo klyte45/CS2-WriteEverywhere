575d1cb1e8b798afd8fa1389750b968de034c87d
Leandro Klyte <klyte45@hotmail.com>
2023-10-29 15:12:59 -0300

    Adding UI

---
+++ BelzontWE.Core/WriteEverywhereCS2Mod.cs
@@ -0,0 +1,41 @@
+﻿#define BURST
+//#define VERBOSE 
+using Belzont.Interfaces;
+using Game;
+using Game.Modding;
+using Game.UI.Menu;
+using System.Collections.Generic;
+
+namespace BelzontWE
+{
+    public class WriteEverywhereCS2Mod : BasicIMod<WEModData>, IMod
+    {
+        public override string SimpleName => "Write Everywhere";
+
+        public override string SafeName => "WriteEverywhere";
+
+        public override string Acronym => "WE";
+
+        public override string Description => "Write Everywhere for Cities Skylines 2";
+
+        public override WEModData CreateNewModData() => new WEModData();
+
+        public override void DoOnCreateWorld(UpdateSystem updateSystem)
+        {
+            updateSystem.UpdateAt<FontServer>(SystemUpdatePhase.Rendering);
+        }
+
+        public override void OnDispose()
+        {
+        }
+
+        public override void DoOnLoad()
+        {
+        }
+
+        protected override IEnumerable<OptionsUISystem.Section> GenerateModOptionsSections()
+        {
+            yield break;
+        }
+    }
+}
---
+++ z_WE_EUIS/WE_EUIS.cs
@@ -0,0 +1,42 @@
+﻿#define LOCALURL
+
+using BelzontWE;
+using K45EUIS_Ext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z_WE_EUIS
+{
+    public class WE_EUIS : IEUISModRegister
+    {
+        public string ModderIdentifier => "k45";
+        public string ModAcronym => "we";
+        public Action<Action<string, object[]>> OnGetEventEmitter => (eventCaller) => WriteEverywhereCS2Mod.Instance.SetupCaller(eventCaller);
+        public Action<Action<string, Delegate>> OnGetEventsBinder => (eventCaller) => WriteEverywhereCS2Mod.Instance.SetupEventBinder(eventCaller);
+        public Action<Action<string, Delegate>> OnGetCallsBinder => (eventCaller) => WriteEverywhereCS2Mod.Instance.SetupCallBinder(eventCaller);
+    }
+    public class WE_EUIS_Main : IEUISAppRegister
+    {
+        public string ModAppIdentifier => "main";
+
+        public string DisplayName => "Write Everywhere - Main";
+
+#if LOCALURL
+        public string UrlJs => "http://localhost:8500/k45-we-main.js";//
+        public string UrlCss => "http://localhost:8500/k45-we-main.css";//
+        public string UrlIcon => $"coui://{WriteEverywhereCS2Mod.Instance.CouiHost}/UI/images/WE.svg";
+#else
+        public string UrlJs => $"coui://{WriteEverywhereCS2Mod.Instance.CouiHost}/UI/k45-we-main.js";
+        public string UrlCss => $"coui://{WriteEverywhereCS2Mod.Instance.CouiHost}/UI/k45-we-main.css";
+        public string UrlIcon => $"coui://{WriteEverywhereCS2Mod.Instance.CouiHost}/UI/images/WE.svg";
+#endif
+
+        public string ModderIdentifier => "k45";
+
+        public string ModAcronym => "we";
+    }
+
+}
