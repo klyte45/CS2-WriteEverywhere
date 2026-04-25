ba4e66bf7db76265fe1fe97f1bd0cca16e565874
Leandro Klyte <klyte45@kwyt.com.br>
2026-04-25 20:27:50 -0300

    Added localization formulas

---
+++ BelzontWE/BuiltinFn/WELocalizationFn.cs
@@ -0,0 +1,24 @@
+﻿using Belzont.Utils;
+using Game.UI.Localization;
+using System.Collections.Generic;
+
+namespace BelzontWE.Builtin
+{
+    [WEBuiltinFunction("Localization")]
+    public static class WELocalizationFn
+    {
+        [WEFormula(typeof(string))]
+        public static string Translate(string str) => LocalizedString.Id(str).Translate();
+
+        [WEFormula(typeof(string))]
+        public static string TranslateWithArgs(string str, Dictionary<string, string> vars)
+        {
+            var result = Translate(str);
+            foreach (var entry in vars)
+            {
+                result = result.Replace("{" + entry.Key + "}", entry.Value);
+            }
+            return result;
+        }
+    }
+}
